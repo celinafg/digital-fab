904a5d24395584108d643031d92db1f9b16bb416
celina garcia <code@koral.one>
2024-03-29 01:24:09 -0700

    convert astro theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 export default defineConfig({
-  base: ".",
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
-  site: "https://celinafg.github.io",
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
