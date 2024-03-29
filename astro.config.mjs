ba8c0cd3317859d175b6b8e1c646e8e287808fdf
celina garcia <code@koral.one>
2024-03-28 18:55:11 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: "https:/celinafg.github.io/digital-fab/",
+  site: "https://celinafg.github.io",
+  base: ".",
   integrations: [mdx(), sitemap(), tailwind()],
 });
