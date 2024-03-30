c1667ece7fe9114071e3f5d470302930d819c7ab
celina garcia <code@koral.one>
2024-03-29 20:11:19 -0700

    fix broken images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://celinafg.github.io/digital-fab",
-  // publicDir: "/digital-fab",
+  publicDir: "/digital-fab",
   base: "/digital-fab",
   integrations: [mdx(), sitemap()],
 });
