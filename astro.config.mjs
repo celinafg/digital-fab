7dc057e397dc015f3dcee10fe1d6ebb7449ab609
celina garcia <code@koral.one>
2024-03-29 19:30:02 -0700

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celinafg.github.io",
+  site: "https://celinafg.github.io/digital-fab",
   base: "digital-fab",
   integrations: [mdx(), sitemap()],
 });
