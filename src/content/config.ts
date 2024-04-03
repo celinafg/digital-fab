0186374d33d0ca4fd5d34dd454b926785e602df6
celina garcia <code@koral.one>
2024-04-03 15:45:08 -0700

    pic links

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
     sourcefile: z.string().optional(),
+    number: z.number().optional(),
   }),
 });
 
